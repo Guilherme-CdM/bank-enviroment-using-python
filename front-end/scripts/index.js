c9ef65c3958ef0550128f5230bff0c3b9e93f822
Lherm <gui.demoraes12@gmail.com>
2026-01-09 21:52:37 -0300

    Atualização_Card

--- front-end/scripts/index.js
+++ front-end/scripts/index.js
@@ -1,3 +1,36 @@
-window.document.addEventListener("DOMContentLoaded", () => {
-  console.log("Front-End script loaded successfully.");
+// ---------------------------------------------
+//               Global Variables
+// ---------------------------------------------
+
+
+
+// ---------------------------------------------
+//               Global Functions
+// ---------------------------------------------
+
+
+
+// ---------------------------------------------
+//            Global Event listeners
+// ---------------------------------------------
+
+
+
+// ---------------------------------------------
+//                  Main Code
+// ---------------------------------------------
+
+console.log('Front-End script loaded successfully.');
+
+window.addEventListener('DOMContentLoaded', () => { // DOM Carregado completamente
+  console.log('DOM fully loaded and parsed');
+    
+  const cardElement = window.document.querySelector('.card');
+
+  cardElement.addEventListener('mouseenter', () => {
+    cardElement.style.transform = 'rotateY(180deg)';
+  });
+  cardElement.addEventListener('mouseleave', () => { 
+    cardElement.style.transform = 'rotateY(0deg)';
+  });
 });
